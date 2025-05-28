2511037868581d1fb031a9b55ead28d1459081c8
Vladimir Eremia <vladimireremia377@gmail.com>
2025-05-28 22:31:55 +0300

    feat: enhance Angular ESLint config with supported file types
    Added `files` property to include supported TypeScript file types in Angular ESLint configuration. Improved alignment with project requirements.

--- configs/angular.js
+++ configs/angular.js
@@ -1,9 +1,11 @@
 import angular from 'angular-eslint';
 import { angularRules } from '../rules/angular.js';
+import { supportedFileTypes } from '../utils/fileTypes.js';
 
 /** @type {import('typescript-eslint').ConfigWithExtends} */
 export const angularConfig = {
   name: 'angular config',
+  files: [supportedFileTypes.ts],
   extends: [...angular.configs.tsRecommended],
   processor: angular.processInlineTemplates,
   rules: {
